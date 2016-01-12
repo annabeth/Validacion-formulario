887fa86a2d1b2afcf54adaf33e518efd9a7ea7c4
karina <kramosq@gmail.com>
2016-01-11 23:53:26 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+$(document).ready(initPage);
+
+function initPage()
+{
+	var opciones = {
+		rules:{
+			nombre:{
+				required:true,
+				minlength:6
+			},
+			apellidos:"required",
+			email:{
+				required:true
+			},
+			telefono:
+			{
+				required:true,
+				minlength:6,
+				maxlength:12
+			},
+            ciudad:
+            {
+                minlength: 3
+            },
+            placa:
+            {
+                minlenght: 6
+            },
+            vehiculo:
+            {
+                required: true,
+                minlenght: 10
+            }
+		},
+		messages:{
+			nombre:"Por favor ingresa tu nombre",
+			apellidos:"Por favor ingresa tu apellido",
+            telefono:"Ingresa tu numero de teléfono",
+			email:{
+				required:"Ingresa tu correo"},
+            ciudad:
+            {
+				required:"Seleccione su cuidad de origen",
+			},
+            placa:
+            {
+                required:"Ingresa tu numero de placa",
+            },
+            vehiculo:
+            {
+                required:"Seleccione el uso del vehículo"
+            }
+
+		},
+		submitHandler: function()
+		{
+			swal("Es válido", "Todo ok", "success");
+		},
+		invalidHandler: function()
+		{
+			swal("Error", "Verifica el fomulario", "warning");
+		}
+	};
+
+
+	$("#myForm").validate(opciones);
+}
